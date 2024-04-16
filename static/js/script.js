45696cfe02a7250977e3ee555fa99c0fd366572b
“hollow288” <571339274@qq.com>
2024-04-16 17:43:19 +0800

    Message board

--- static/js/script.js
+++ static/js/script.js
@@ -1,41 +1,7 @@
-/*================ 
-Template Name: Artivell
-Description:  Architecture and Interior Designer Page HTML Template
-Version: 1.0 
-Author: https://www.templatemonster.com/authors/techeshta
-=======================*/
 
-
-// Table Of Content
-
-// 02. Popup
-// 02. Progress Bar 
-// 03. Counter
-// 04. Testimonial Slider 
-// 05. Wow js
-// 06. Form Submitting 
 /* -----------------------------------------------------
     01. Popup
 ----------------------------------------------------- */
-$(document).ready(function() {
-    if ($.fn.magnificPopup) {
-        $('.popup-video').magnificPopup({
-            type: 'iframe'
-        });
-
-        $.extend(true, $.magnificPopup.defaults, {
-            iframe: {
-            patterns: {
-                youtube: {
-                index: 'youtube.com/',
-                id: 'v=',
-                src: 'https://www.youtube.com/embed/%id%?autoplay=1'
-                }
-            }
-            }
-        });
-    }
-});
 // Popup with Slider
 Fancybox.bind('[data-fancybox="gallery"]', {
     Thumbs: false,
@@ -117,41 +83,3 @@ $(".testimonial-wrapper").slick({
 ----------------------------------------------------- */
 new WOW().init();
 
-/* -----------------------------------------------------
-    06. Form Submitting 
------------------------------------------------------ */
-
-var form = document.getElementById("contact-form");
-    
-async function handleSubmit(event) {
-    event.preventDefault();
-
-    var status = document.getElementById("contact-form-status");
-
-    var data = new FormData(event.target);
-
-    fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-    }).then(response => {
-    if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-    } else {
-        response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-            status.innerHTML = "Oops! There was a problem submitting your form"
-        }
-        })
-    }
-    }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
-    });
-   
-}
-form.addEventListener("submit", handleSubmit)
